312fd2f4e4250283f65b4ca5046f98258789e258
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2021-03-10 21:20:16 +0200

    here

---
+++ src/commands.ts
@@ -0,0 +1,4 @@
+export const commands = [
+    "Tell me about",
+    "Picture of"
+]
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import express from 'express';
 import bodyParser from 'body-parser';
-import wiki from 'wikijs';
+import express from 'express';
 import localtunnel from 'localtunnel';
-import gis from 'g-i-s';
-import { apiUrl, token } from './config'
+import { commands } from './commands';
+import { apiUrl, token } from './config';
+import { _tellMeAbout } from './commands/tell-me-about'
+import { _pictureOf } from './commands/picture-of'
 
 const app = express();
 app.use(bodyParser.json());
@@ -40,51 +41,27 @@ app.post('/', function (req, res) {
   const data = req.body.messages[0];
   console.log(data);
 
+  const command = commands.find(command => {
+    if (data.body.startsWith(command)) return command;
+  });
 
+  console.log(command);
 
-  if (data && data.body && data.body.match(/^Tell me about /g)) {
-    const about = data.body.replace(/^Tell me about /g, '');
-    console.log(about);
-    wiki()
-      .page(about)
-      .then(page => page.summary())
-      .then(summary => {
-        axios.post(`${apiUrl}/sendMessage?token=${token}`, {
-          body: summary,
-          chatId: data.chatId
-        });
-      });
+  switch (command) {
+    case 'Tell me about':
+      _tellMeAbout(data);
+      break;
+
+    case 'Picture of':
+      _pictureOf(data);
+      break;
+
+    default:
+      break;
   }
 
-
-  if (data && data.body && data.body.match(/^Picture of /g)) {
-    axios.post(`${apiUrl}/sendMessage?token=${token}`, {
-      body: 'Hang tight...',
-      chatId: data.chatId
-    });
-
-    const about = data.body.replace(/^Picture of /g, '');
-    console.log(about);
-
-    gis(about, (err: any, res: any) => {
-      console.log(res[0]);
-      const _body = res[Math.floor(Math.random() * 100)].url;
-      console.log('Req body: ', _body);
-      const newUrl = _body.match(/^(.+?\.(png|jpe?g))/i)[0];
-      console.log('newUrl', newUrl)
-      axios.post(`${apiUrl}/sendFile?token=${token}`, {
-        filename: newUrl,
-        body: newUrl,
-        message: about,
-        chatId: data.chatId
-      }, {
-        headers: {
-          'Content-type': 'application/json'
-        }
-      });
-
-
-    });
-  }
   res.end();
 });
+
+
+
